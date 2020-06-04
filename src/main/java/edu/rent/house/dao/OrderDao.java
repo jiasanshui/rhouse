6846edc6527d0cc7864a0e8d4e11745f0278da88
wlc1998 <1158267345@qq.com>
2020-06-04 21:03:29 +0800

    12345

--- src/main/java/edu/rent/house/dao/OrderDao.java
+++ src/main/java/edu/rent/house/dao/OrderDao.java
@@ -10,8 +10,8 @@ import java.util.Map;
 @Repository
 public interface OrderDao {
 
-    @Insert("insert into t_order(order_num,user_id,house_id,start_time,end_time,time,price,status) " +
-            "values(#{orderNum},#{userId},#{houseId},#{startTime},#{endTime},#{price},1)")
+    @Insert("insert into t_order(order_num,user_id,house_id,start_time,end_time,days,price,status) " +
+            "values(#{orderNum},#{userId},#{houseId},#{startTime},#{endTime},#{days},#{price},1)")
     Integer saveOrder(Map map);
 
     @Update("update t_order set status=0 where id=#{id}")
